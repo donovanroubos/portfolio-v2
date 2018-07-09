9831aa186190abca497f1e20ed5b114a3473ac4b
Donovan Roubos <hello@donovanroubos.nl>
2018-07-09 22:49:23 +0200

    Add document SCSS importing and set variables, also add navigation

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,47 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div>
+    <transition>
+      <header class="container-l header">
+        <router-link to="/" tag="div"><a class="logo">Donovan Roubos</a></router-link>
+        <nav class="navigation">
+            <router-link to="/work">Work</router-link>
+            <router-link to="/about">About</router-link>
+            <router-link to="/#contact">Contact</router-link>
+        </nav>
+      </header>
+      <router-view/>
+    </transition>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+<style lang="scss" scoped>
+.header {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  height: 24px;
+  margin-top: 56px;
+  align-items: center;
+
+  .logo {
+    font-size: 21px;
+    color: $black;
+    font-weight: 400;
+    text-decoration: none;
+  }
 }
-#nav {
-  padding: 30px;
+.navigation {
+  text-align: right;
+
+  a {
+    color: $black;
+    font-size: 18px;
+    padding: 0px 16px;
+    text-decoration: none;
+
+    &.router-link-exact-active {
+      text-decoration: underline;
+    }
+  }
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,7 @@
 <template>
- <div class="">
-   <h1>Recent work</h1>
-   <p>Phasellus posuere mauris quis lorem vulputate aliquam. Proin convallis felis et finibus hendrerit. Maecenas ultrices nunc a sapien maximus, eu condimentum leo placerat. Nam a diam congue lectus ullamcorper ultricies non id enim. In quis erat fermentum nibh rutrum consectetur.</p>
+  <div class="">
 
- </div>
+  </div>
 </template>
 
 <script>
@@ -16,11 +14,4 @@ export default {
 </script>
 
 <style scoped lang="scss">
-p {
-  max-width: 600px;
-  text-align: left;
-  margin: 0 auto;
-  line-height: 32px;
-  color: #4F5059;
-}
 </style>
